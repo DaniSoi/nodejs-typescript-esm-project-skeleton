04bbe8860687810ddd69459c1ac39c1fcdab3950
Daniel Soifer <danisoifer@gmail.com>
2023-08-22 17:18:23 +0300

    .

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ export default {
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.(m)?js$": "$1",
   },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(m)?ts$",
+  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(m)?ts$",
   coverageDirectory: "coverage",
   collectCoverageFrom: [
     "src/**/*.ts",
