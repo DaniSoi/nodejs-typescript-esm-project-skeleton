4e152c5dfdd873e2afb29048805ba39d7d2fbcfd
Daniel Soifer <danisoifer@gmail.com>
2023-08-25 10:32:27 +0300

    added comment about es modules mocking in jest

--- tests/api/home/home.controller.test.ts
+++ tests/api/home/home.controller.test.ts
@@ -3,6 +3,45 @@ import supertest, { SuperAgentTest } from "supertest"
 import { createApp } from "../../../src/app.js"
 import { config } from "../../../src/config/index.js"
 
+/*
+    Axios mock example with ES modules in TypeScript:
+
+    jest.unstable_mockModule("axios", () => {
+      const originalModule = jest.requireActual<typeof import("axios")>("axios")
+      return {
+        default: {
+          get: jest.fn(),
+        },
+        isAxiosError: originalModule.isAxiosError,
+      }
+    })
+
+    const { default: axios } = await import("axios")
+    const { createApp } = await import("../../../src/app.js")
+
+    const mockedAxios = jest.mocked(axios)
+
+    -------------------
+    Usage example:
+
+    it("should return 200 OK", async () => {
+      mockedAxios.get.mockResolvedValueOnce({
+        data: {
+          example: "example",
+        },
+      })
+
+      ...
+    })
+
+    -------------------
+    Reset mock:
+
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+ */
+
 const initAgent = async (): Promise<SuperAgentTest> => {
   // instead of importing config, we can mock it
   const app = createApp(config)
